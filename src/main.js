9f7dae48f72a3f000f427f298a0ea692d980ce24
CUBBY-SIL <nikita.bergen0@gmail.com>
2023-12-06 21:55:26 +0700

    Подключение скрипта для меню и библиотеки AOS.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,13 @@
 import './index.html';
 import './index.scss';
+import './modules/dropDownMenu';
+import * as AOS from 'aos';
+import 'aos/dist/aos.css';
 
+window.addEventListener("load", () => {
+    AOS.init({
+        duration: 700,
+        offset: 100,
+        once: true,
+    });
+});
